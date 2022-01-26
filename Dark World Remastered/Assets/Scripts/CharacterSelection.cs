8637cc051305c5a32a87440cfcc61ebe52bbb888
Bjay <61071579+lelazeb123@users.noreply.github.com>
2022-01-26 19:01:40 +0800

    Gun stats added

--- Dark World Remastered/Assets/Scripts/CharacterSelection.cs
+++ Dark World Remastered/Assets/Scripts/CharacterSelection.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class CharacterSelection : MonoBehaviour
 {
+    public GameObject[] panels;
     public GameObject[] characters;
     public int selectedCharacter = 0;
+    public int panelCount = 0;
+
+
+    public static string charName = "Kyrilios";
 
     public void NextCharacter()
     {
@@ -26,9 +32,33 @@ public class CharacterSelection : MonoBehaviour
         characters[selectedCharacter].SetActive(true);
     }
 
+    public void NextPanel()
+    {
+        panels[panelCount].SetActive(false);
+        panelCount = (panelCount + 1) % panels.Length;
+        panels[panelCount].SetActive(true);
+    }
+
+    public void PreviousPanel()
+    {
+        panels[panelCount].SetActive(false);
+        panelCount--;
+        if(panelCount < 0)
+        {
+            panelCount += panels.Length;
+        }
+        panels[panelCount].SetActive(true);
+    }
+
+    public void SelectCharacter()
+    {
+        charName = characters[selectedCharacter].name;
+    }
+
     public void StartGame()
     {
         PlayerPrefs.SetInt("selectedCharacter", selectedCharacter);
         SceneManager.LoadScene(1, LoadSceneMode.Single);
     }
+
 }
